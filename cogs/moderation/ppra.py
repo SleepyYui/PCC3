4efc4c9829ea15944217cb9ed40cf233d2e0e082
YES [German] <53976729+YES-German@users.noreply.github.com>
2022-09-02 20:58:34 +0200

    Update ppra.py

--- cogs/moderation/ppra.py
+++ cogs/moderation/ppra.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord.commands import permissions
 from discord import Option
-
+from discord import default_permissions
 #rolelist = [589435378147262464, 632674518317531137]
 
 class ppra(commands.Cog):
@@ -11,6 +11,7 @@ class ppra(commands.Cog):
         self.client = client
 
     @commands.slash_command(name="ppra", description="Moderator only", guild_ids=[571031703661969430, 708218806928932896])
+    @default_permissions(kick_members=True)
     async def ppra(self, ctx, member: Option(discord.Member, required=True)):
         role = ctx.guild.get_role(775736993018806322)
         await ctx.respond(f"Added the **PRO PLAYER** role to **{member}**", delete_after=10)
