edc9feff1b4280434d2a44db4413eefc0ae002b7
Laptop <98688203+LaptopCat@users.noreply.github.com>
2023-02-20 15:40:15 +0200

    Update pcc2_group.py
    Added a new command - /pcc2 user

--- cogs/command_groups/pcc2_group.py
+++ cogs/command_groups/pcc2_group.py
@@ -3,8 +3,7 @@ from discord import SlashCommandGroup
 from discord.ext import commands
 import discord
 from discord import Option
-
-from cogs.pc_creator_commands.record_pcc2_group import record_pcc2
+from cogs.pc_creator_commands.record_pcc2_group import record_pcc2, pcc2_user
 
 
 class pcc2_group(commands.Cog):
@@ -19,7 +18,10 @@ class pcc2_group(commands.Cog):
     @pcc2.command(name="record", description="Shows the best PC in PCC2")
     async def record_group(self, ctx):
         await record_pcc2(ctx)
-
+    
+    @pcc2.command(name="user", description="Get info on a user from a trade code (ID)")
+    async def user(self, ctx, code: Option(int, name="code", description="Trade code (ID)", min_value=100000, max_value=999999)):
+        await pcc2_user(ctx, code)
 
 def setup(client):
     client.add_cog(pcc2_group(client))
