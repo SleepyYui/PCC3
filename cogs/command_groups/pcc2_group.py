fb3d4fb107cc4f88ec4b7c266877c071ad222a2f
Yui <dev@sleepyyui.com>
2023-06-22 14:24:01 +0200

    Update pcc2_group.py

--- cogs/command_groups/pcc2_group.py
+++ cogs/command_groups/pcc2_group.py
@@ -3,7 +3,7 @@ from discord import SlashCommandGroup
 from discord.ext import commands
 import discord
 from discord import Option
-from cogs.pc_creator_commands.record_pcc2_group import record_pcc2, pcc2_user
+from cogs.pc_creator_commands.record_pcc2_group import record_pcc2, pcc2_user, pcc2_status
 
 
 class pcc2_group(commands.Cog):
@@ -22,6 +22,10 @@ class pcc2_group(commands.Cog):
     @pcc2.command(name="user", description="Get info on a user from a trade code (ID)")
     async def user(self, ctx, code: Option(int, name="code", description="Trade code (ID)", min_value=100000, max_value=999999)):
         await pcc2_user(ctx, code)
+
+    @pcc2.command(name="status", description="Shows the status of PCC2")
+    async def status(self, ctx):
+        await pcc2_status(ctx)
 
 def setup(client):
     client.add_cog(pcc2_group(client))
