b0c8215c9200a35d2a30b173905f037c24b12131
YES [German] <53976729+YES-German@users.noreply.github.com>
2023-12-20 22:18:28 +0100

    Fixed a typo

--- cogs/pc_creator_commands/customcpu_group.py
+++ cogs/pc_creator_commands/customcpu_group.py
@@ -7,7 +7,7 @@ from discord import Option
 #command -> pcc1_group
 
 async def customcpu(ctx):
-    embed = discord.Embed(title="~~This is how custom CPUs where made~~", color=13565696)
+    embed = discord.Embed(title="This is how custom CPUs were made", color=13565696)
     embed.set_image(url="https://images-ext-2.discordapp.net/external/uADQCXXNQ6xx3QHW8_cPLJ8w0kGUg1oiQDmxSVAvjGQ/https/media.discordapp.net/attachments/571031705109135361/809885303119675482/Screenshot_2021-02-12-17-34-34-411_com.ultraandre.pccreator.jpg?width=930&height=441")
     embed.set_footer(text="You cannot make custom CPUs anymore. The feature to manufacture them was removed in an update")
     await ctx.respond(embed=embed)
